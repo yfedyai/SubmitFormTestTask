7f3ffc6da092ddc855f4a9d64e7b9453446521a3
Yaroslav Fedyai <yarkofedyai@gmail.com>
2019-09-12 18:09:27 +0300

    store setup

---
+++ src/store/form.js
@@ -0,0 +1,32 @@
+import axios from "axios"
+
+export default {
+    state: {
+        data: null
+    },
+
+    mutations: {
+        setData(payload, state) {
+            state.data = payload
+        }
+    },
+    actions:{
+        async getData({commit}){
+            try {
+                const response = await axios.get("http://localhost:3000/form") 
+                console.log(response.data)
+                commit('setData', response.data)
+            }
+            catch(err){
+                console.log(err)
+            }
+        }
+        
+
+    },
+    getters:{
+        
+    }
+
+    
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import Form from '../store/form'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
+    Form
   }
 })
