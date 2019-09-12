e29b88def24041fcf240d9445df60228d343f712
Yarik Fedyai <yarkofedyai@gmail.com>
2019-09-12 22:58:06 +0300

    fixed module name

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import Form from '../store/form'
+import form from '../store/form'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    Form
+    form
   }
 })
